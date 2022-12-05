d9a8e648b725ecd4e706ad3e637b29c8b169874f
ian Ding <ianding@ians-MacBook-Pro.local>
2022-12-05 09:31:21 +0800

    minor update

--- model/database_v2.js
+++ model/database_v2.js
@@ -43,50 +43,6 @@ exports.raw_query = function (query, db = process.env.DATABASE) {
     })
 }
 
-function ConnectDatabase(host, user, password, database) {
-    // var con = mysql.createPool({
-    //     host: host,
-    //     user: user,
-    //     password: password,
-    //     database: database,
-
-    // });
-
-    // con.getConnection((err, connection) => {
-    //     if (err)
-    //         throw err;
-    //     console.log('Database connected successfully');
-    //     connection.release();
-    // });
-
-    // var con = mysql.createConnection({
-    //     host: host,
-    //     user: user,
-    //     password: password,
-    //     database: database
-    // });
-
-    // con.connect(function (err) {
-    //     if (err) throw err;
-    // });
-
-    // return con;
-}
-
-const db = ConnectDatabase(process.env.HOSTNAME, process.env.USER_ID, process.env.PASSWORD);
-
-function raw_query(query, callback) {
-
-    return new Promise(function (resolve, reject) {
-        db.query(query.toString(), function (error, results, fields) {
-            if (error) {
-                reject(error)
-            }
-            resolve(results)
-        });
-    });
-}
-
 
 
 
