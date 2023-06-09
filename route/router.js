33bd37dc63abe2215c4ab4f5c117f3202c707ecb
ding0215 <89462467+ding0215@users.noreply.github.com>
2023-06-09 11:01:19 +0800

    Update router.js

--- route/router.js
+++ route/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const router_v2 = require('./router_v2')
+const fnc = require('./function')
 
 router.use('/router_v2', router_v2)
+router.use('/function', fnc)
 
 module.exports = router;
 
